47f1bf789028fbcc3a037d780620e568ae755721
Yago Uran Kurashiki Rios <145205598+Yajoojj@users.noreply.github.com>
2025-04-03 21:59:47 -0300

    Add files via upload

---
+++ Atividades-Avulsas/livro/main.c
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Livro {
+  char titulo[30];
+  int year;
+  int paginas;
+  float valor;
+}L;
+
+int main() {
+
+    L livros[5];
+    int i;
+    int media;
+
+    for (i = 0; i < 5; i++) {
+        printf("Informe o nome do titulo do livro: \n");
+        scanf("%s",livros[i].titulo);
+        printf("Informe o ano de lançamento do Livro: \n");
+        scanf("%d",&livros[i].year);
+        printf("Informe a quantidade de paginas do livro: \n");
+        scanf("%d",&livros[i].paginas);
+        printf("Informe o valor do livro: \n");
+        scanf("%f",&livros[i].valor);
+    }
+
+
+    for (i=0;i<5;i++) {
+        media +=livros[i].paginas;
+    }
+    media = media/5;
+    printf("Media de paginas = %d\n",media);
+
+
+    return 0;
+}
---
+++ Atividades-Avulsas/pesquisa-habitantes/main.c
@@ -0,0 +1,45 @@
+﻿#include <stdio.h>
+
+#define MAX_HABITANTES 500
+
+typedef struct {
+    int idade;
+    char sexo;
+    float salario;
+    int num_filhos;
+} H;
+
+void coletarDados(H habitantes[]) {
+    for (int i = 0; i < MAX_HABITANTES; i++) {
+        printf("\nHabitante %d:\n", i + 1);
+        printf("Idade: ");
+        scanf("%d", &habitantes[i].idade);
+        printf("Sexo (M/F): ");
+        scanf(" %c", &habitantes[i].sexo);
+        printf("Salário: ");
+        scanf("%f", &habitantes[i].salario);
+        printf("Número de filhos: ");
+        scanf("%d", &habitantes[i].num_filhos);
+    }
+}
+
+
+float calcularMediaSalarial(H habitantes[]) {
+    float soma = 0;
+    for (int i = 0; i < MAX_HABITANTES; i++) {
+        soma += habitantes[i].salario;
+    }
+    return soma / MAX_HABITANTES;
+}
+
+int main() {
+    H habitantes[MAX_HABITANTES];
+
+    printf("Digite os dados de %d habitantes:\n", MAX_HABITANTES);
+    coletarDados(habitantes);
+
+    float media_salario = calcularMediaSalarial(habitantes);
+    printf("\nMédia salarial: %.2f\n", media_salario);
+
+    return 0;
+}
